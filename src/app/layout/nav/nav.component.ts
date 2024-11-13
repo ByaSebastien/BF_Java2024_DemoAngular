03b5ae8d74ecc7b9111dc6f351a15b30b1f113ab
Bya Sébastien <sebastien.bya@bstorm.be>
2024-11-13 10:32:00 +0100

    Demo directive

--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Link} from '../../shared/models/link';
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +7,54 @@ import { Component } from '@angular/core';
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
+  links: Link[] = [
+    {
+      title: 'Home',
+      url: '/home'
+    },
+    {
+      title: 'About',
+      url: '/about'
+    },
+    {
+      title: 'Demo',
+      isChildrenVisible: false,
+      children: [
+        {
+          title: 'Bindings',
+          url: '/demo/demo01'
+        },
+        {
+          title: 'Pipes',
+          url: '/demo/demo02'
+        },
+        {
+          title: 'Directives',
+          url: '/demo/demo03'
+        },
+      ],
+    },
+    {
+      title: 'Exo',
+      isChildrenVisible: false,
+      children: [
+        {
+          title: 'Timer',
+          url: 'exo/exo01'
+        }
+      ],
+    },
+  ];
 
+  toggleIsVisible(link: Link) {
+    if(!link.children){
+      return;
+    }
+    for(let l of this.links){
+      if(l !== link){
+        l.isChildrenVisible = false;
+      }
+    }
+    link.isChildrenVisible = !link.isChildrenVisible;
+  }
 }
