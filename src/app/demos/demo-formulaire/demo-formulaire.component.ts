c9eb017ca983c9e6b3883bf979b41a18354d611c
Bya Sébastien <sebastien.bya@bstorm.be>
2024-11-14 13:38:04 +0100

    Demo formulaire

--- src/app/demos/demo-formulaire/demo-formulaire.component.ts
+++ src/app/demos/demo-formulaire/demo-formulaire.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Person} from '../../shared/models/person';
+import {beforeToday} from '../../shared/validators/before-today';
+import {samePassword} from '../../shared/validators/same-password';
 
 @Component({
   selector: 'app-demo-formulaire',
@@ -17,7 +19,7 @@ export class DemoFormulaireComponent {
     this.personForm = this._fb.group({
       firstName: [null, [Validators.required,Validators.minLength(2), Validators.maxLength(123)]],
       lastName: [null, [Validators.required,Validators.minLength(2), Validators.maxLength(80)]],
-      birthDate: [null,[Validators.required]],
+      birthDate: [null,[Validators.required,beforeToday()]],
       email: [null, [Validators.required,Validators.email]],
       password: [null, [Validators.required,Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-=]).{8,}$/)]],
       confirmPassword: [null, [Validators.required]],
@@ -25,7 +27,7 @@ export class DemoFormulaireComponent {
       siblings: [0,[Validators.required,Validators.min(0)]],
       country: [null, [Validators.required]],
       single: [false,[]],
-    });
+    }, {validators: samePassword()});
   }
 
   submit() {
